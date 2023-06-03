33f24625f5680c4211e57153fd8218d0174c8286
mi-gongan <kib0202@naver.com>
2023-06-04 03:34:46 +0900

    FIX

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -10,7 +10,7 @@ export const account = '0xfs312a2f3E829C0b614566B3E152e417d14q6EP3';
 
 export const challengeList: Array<ChallengeType> = [
   {
-    id: '24',
+    id: process.env.NEXT_PUBLIC_CHALLENGE_ID!,
     deposit: 3334,
     title: 'Participating in \nETH SEOUL',
     image: '/images/dummy/challenges/challenge_ethseoul.png',
